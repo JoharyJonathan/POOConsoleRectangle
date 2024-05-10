b82b1c97f0e37d461f8f612633677cb66d155b6c
JoharyJonathan <rjoharyjonathan@gmail.com>
2024-05-10 12:48:53 +0300

    Initial commit

---
+++ ConsoleRectangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Rectangle rec = new Rectangle(120.23f, 35.1f);
+            rec.Perimetre();
+            rec.Surface();
+            rec.Color();
+
+            Console.WriteLine();
+
+            Rectangle rect = new Rectangle(150.654f, 45.0045f, "bleu");
+            rect.Perimetre();
+            rect.Surface();
+            rect.Color();
+
+            Console.WriteLine();
+            Console.WriteLine(rect.Couleur);
+
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleRectangle/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleRectangle
+{
+    class Rectangle
+    {
+        private float longueur;
+        private float largeur;
+        private string couleur;
+
+        public string Couleur
+        {
+            get
+            {
+                return couleur;
+            }
+        }
+
+        public Rectangle(float Longueur, float Largeur)
+        {
+            longueur = Longueur;
+            largeur = Largeur;
+        }
+
+        public Rectangle(float Longueur, float Largeur, String Couleur)
+        {
+            longueur = Longueur;
+            largeur = Largeur;
+            couleur = Couleur;
+        }
+
+        public void Perimetre()
+        {
+            float perimetre = (2 * longueur) + (2 * largeur);
+            Console.WriteLine("Le perimetre du rectangle est " + perimetre );
+        }
+
+        public void Surface()
+        {
+            Console.WriteLine("La surface du rectangle est " + (longueur * largeur));
+        }
+
+        public void Color()
+        {
+            if (Couleur != null)
+                Console.WriteLine("La couleur du rectangle est " + Couleur);
+            else
+                Console.WriteLine("Aucune couleur");
+        }
+    }
+}
